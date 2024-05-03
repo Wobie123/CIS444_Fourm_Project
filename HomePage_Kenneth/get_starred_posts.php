1d87262f96f76075e4e8b3b138aaa3090b95748e
Wobie123 <kennethha47@gmail.com>
2024-05-02 18:31:12 -0700

    getting starredpost

---
+++ HomePage_Kenneth/get_starred_posts.php
@@ -0,0 +1,60 @@
+<?php
+// Include the database connection
+include 'config.php';
+
+// Email of the user
+$user_email = "ha134@gmail.com";
+
+// SQL query to retrieve starred posts for the given user along with their tags
+$sql = "SELECT Post.*, GROUP_CONCAT(Tag.TagName) AS Tags
+        FROM Post
+        INNER JOIN UserStarredPost ON Post.PostID = UserStarredPost.PostID
+        LEFT JOIN PostTag ON Post.PostID = PostTag.PostID
+        LEFT JOIN Tag ON PostTag.TagName = Tag.TagName
+        WHERE UserStarredPost.Email = '$user_email'
+        GROUP BY Post.PostID";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        // Extracting post information
+        $post_id = $row["PostID"];
+        $title = $row["Title"];
+        $body = $row["Body"];
+        $post_date = $row["PostDate"];
+        $tags = $row["Tags"];
+
+        // Formatting date
+        $formatted_date = date("m/d/y", strtotime($post_date));
+
+        // Outputting HTML for each post
+        echo '<div class="post">';
+        echo '<a href="../Forum_View/forumview.html" class="post-link">';
+        echo '<div class="post-container" id="">';
+        echo '<div class="post-image"><img src="images/profile-pic.png" alt=""></div>';
+        echo '<div class="post-content">';
+        echo '<div class="post-tag">';
+        echo "<p>$tags</p>"; // Displaying all tags associated with the post
+        echo '</div>';
+        echo '<div class="post-header">';
+        echo "<h4 id='post-title'>$title</h4>";//tittle of the discription
+        echo '</div>';
+        echo "<p id='post-description'>$body</p>";//body of the discription
+        echo '<div class="post-date">';
+        echo "<p>Posted: $formatted_date</p>";//display posted date with format
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '</a>';
+        echo '<input type="checkbox" id="star-checkbox">';
+        echo '<label for="star-checkbox"class="star-checkbox">></label>';
+        echo '</div>';
+    }
+} else {
+    echo "you have no starred posts";
+}
+
+$conn->close();
+?>
