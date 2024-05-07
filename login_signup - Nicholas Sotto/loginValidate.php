3a35e479be7b1b269cf3299c5d9263216ef65c42
nicksotto <123523889+nicksotto@users.noreply.github.com>
2024-05-06 22:07:25 -0700

    Update loginValidate.php

--- login_signup - Nicholas Sotto/loginValidate.php
+++ login_signup - Nicholas Sotto/loginValidate.php
@@ -1,20 +1,6 @@
 <!-- PHP File for the Log In Page -->
 
 <!-- NEED TO SET UP CONNECTION TO THE DATA BASE -->
-	<?php
-		$host = 'localhost';
-		$username = 'team_6';
-		$password = 'a06h1ld3';
-		$dbname = 'team_6';
-
-
-
-		if ($conn->connect_error) {
-			die("Connection Failed: " . $conn->connect_error);
-		}
-
-	?>
-
 
 <!doctype html>
 <html lang = "en">
@@ -36,14 +22,8 @@
 		</div>
 	</header>
 	
-	
+	<form id = "login" action="../HomePage/HomePage.php" class = "login-background" method = "get">
 	<?php
-	
-		session_start();
-		$Email = $_GET['email'];
-		$_SESSION['email'] = $Email;
-		//retriving data input from login page
-		// username and password
 		$User_name = $_GET['user'];
 		$Pswd = $_GET['pswd'];
 		//checking to see if any of the text box is empty
@@ -53,35 +33,48 @@
 			exit("<p> Please enter an email and password to login. Please go back to return to the login page.</p>"); 
 			//displays this message to go back to attept to sign in
 	
-		//attept to connec to database
-		try {
-			$conn = new mysqli($host, $username, $password, $dbname);
+		//attept to connect to database
+		//db info
+		$servername = "localhost";
+		$username = "team_6";
+		$password = "a06h1ld3";
+		$database = "team_6";
+
+		// create connection
+		$conn = new mysqli($servername, $username, $password, $database);
+
+		// check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}else{
+			//echo("<p>inside Connection Successful</p>");
+			//return;
 		}
-		//will display the error message and code if it the database does not connect
-		catch (Exception $exc) {
-			$connerror = $exc->getMessage();
-			$connerrno = $exc->getCode();
-		}
+		
 		
 		//check database to see if username and password match
 		$TableName = "User";
-		$sqlvalidate = "SELECT * FROM $TableName WHERE Username ='$User_name' 
-					AND Password='$Pswd'";
-		$QueryResult = $conn->query($SQLstring);
-	
+		$sqlvalidate = "SELECT * FROM $TableName WHERE Username ='$User_name'"; 
+		//AND Password='$Pswd'";
+		$QueryResult = $conn->query($sqlvalidate);
+		
 		//if number of rows = 0, results return none so will display the message below
 		if($QueryResult->num_rows == 0)
-			die("<p>You must enter a valid username. Please try again.</p>");
+			die("<p>You must enter a valid username. Please go back to return to the login page.</p>");
+		//Grabs the row numbers of teh matched user
+		$Row = $QueryResult->fetch_row();
+		if ($Pswd != $Row[4])
+			die("<p>The username and password do not match. Please go back to return to the login page.");
 		//successful to match a row, will close the database
-		else $username = $Row[0];
-		$conn->close();
+		//else echo("<p> Username and Password matched! </p>");
+		$conn->close();  
+		
 	?>
 	
-	
-	<form action="NEED PHP FOR HOME PAGE.php" class = "login-background" method = "get">
-	<h2> Welcome back, <? $username ?> !</h2>
-	<p> <input type = "hidden" name="user" value ="<?= $username ?>"</p>
-		<input type ="submit" value = "Continue to Homepage" /> </p>
-	</form>
+	<h2> Welcome back, <?= $User_name ?>!</h2>
+	<p>
+		<input type ="submit" value = "Continue to Homepage" />
+	</p>
+	</form>	
 </body>
 </html>
